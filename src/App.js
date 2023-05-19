a16e29c83be8eb04337733d1970627007f79cc82
Ananya Tripathy <tripathykhushi2002@gmail.com>
2023-05-19 20:27:57 +0530

    Filter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-// import logo from './logo.svg';
+import logo from './Assets/MainLogo.png';
 import './App.css';
-import logo from "./Assets/MainLogo.png"
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import React from 'react';
+import JsonData from './Components/JSONdata/JSONdata';
 
-// import CustomWidthTooltip from '@mui/material/CustomWidthTooltip';
-import { useState, useEffect } from "react";
-// import API from "./Services/axios";
-import axios from "axios";
-function App() {
-
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get(
-        "https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json"
-      )
-      .then((data) => {
-        setData(data.data.employees);
-      });
-  }, []);
-
-  const CustomWidthTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))({
-    [`& .${tooltipClasses.tooltip}`]: {
-      maxWidth: 500,
-    },
-  });
-
-  console.log(data)
+const App = () => {
   return (
     <div>
       <div className='bg-[#0072bc] w-screen'>
@@ -38,43 +11,9 @@ function App() {
           <img src={logo} className='object-contain'/>
         </div>
       </div>
-      <div className="flex justify-center items-center pt-[5vw]">
-          <div className="flex space-x-1">
-              <input
-                  type="text"
-                  className="block w-full px-4 py-2 text-[#0072bc] bg-white border rounded-full focus:border-[#0072bc] focus:ring-[#0072bc] focus:outline-none focus:ring focus:ring-opacity-40"
-                  placeholder="Search Employee"
-              />
-              <button className="px-4 text-white bg-[#0072bc] rounded-full ">
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-5 h-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                  >
-                  <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                  </svg>
-              </button>
-        </div>
-      </div>
-      <div>
-        {data&&data.map((item,idx)=>{
-          return (
-          <div className="pl-10">
-            <CustomWidthTooltip title={item.designation}>
-              <Button sx={{ m: 1 }}>{item.name}</Button>
-            </CustomWidthTooltip>
-          </div>)
-        })}
-      </div>
+      <JsonData />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/JSONdata/JSONdata.js
@@ -0,0 +1,259 @@
+import React, { useState, useEffect } from 'react';
+
+const JsonData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [searchName, setSearchName] = useState('');
+  const [filterDesignation, setFilterDesignation] = useState('');
+
+  useEffect(() => {
+    fetchJsonData();
+  }, []);
+  
+  console.log(data)
+  const fetchJsonData = async () => {
+    try {
+      const response = await fetch('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json');
+      const jsonData = await response.json();
+      setData(jsonData.employees);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching JSON data:', error);
+    }
+  };
+
+  const [search, setSearch] = React.useState("");
+
+  const searchFunction = (name) => {
+    let tempData = data.filter((item) => {
+      return item.name;
+    });
+    console.log(tempData);
+    let temp = tempData.filter((item) => {
+      console.log(item.name, name);
+      return item.name.toLowerCase().includes(name.toLowerCase());
+    });
+    // setFilteredData(temp);
+    console.log(temp);
+    return temp;
+  };
+
+  const [filteredData, setFilteredData] = React.useState(data);
+
+  const handleSearch = (e) => {
+    setFilteredData(searchFunction(search));
+  };
+
+  const clearSearch = () => {
+    setFilteredData(data);
+  };
+
+  useEffect(() => {
+    if(data){
+
+        setFilteredData(data);
+    }
+  }, [data]);
+
+  const [designation, setDesignation] = React.useState("");
+  const [skills, setSkills] = React.useState("");
+
+  const [dataDesignations, setDataDesignations] = React.useState([]);
+  const [dataSkills, setDataSkills] = React.useState(["Python", "Java"]);
+
+  const handleDesignationFilter = (e) => {
+    if (designation == "Clear") {
+      setFilteredData(data);
+      return;
+    } else {
+      let tempData = data.filter((item) => {
+        return item.designation;
+      });
+      console.log(tempData);
+      let temp = tempData.filter((item) => {
+        console.log(item.designation, designation);
+        return item.designation
+          .toLowerCase()
+          .includes(designation.toLowerCase());
+      });
+      setFilteredData(temp);
+    }
+  };
+
+  useEffect(()=>{
+    if(designation){
+        handleDesignationFilter();
+    }
+  },[designation])
+  useEffect(() => {
+    if(data){
+    let tempData = data.filter((item) => {
+      return item.designation;
+    });
+    let temp = tempData.map((item) => {
+      return item.designation;
+    });
+    setDataDesignations([...new Set(temp)]);
+}
+  }, [data]);
+
+  return (
+    <div className="container mx-auto p-4">
+      {loading ? (
+        <div className="text-center">Loading...</div>
+      ) : (
+        <div>
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center space-x-4">
+              <label htmlFor="searchName" className="font-medium">Search by Name:</label>
+              <input
+                id="searchName"
+                type="text"
+                value={search}
+                onChange={(e)=>{setSearch(e.target.value)}}
+                className="border border-gray-300 px-2 py-1 rounded"
+              />
+              <div onClick={handleSearch} className='cursor-pointer'>Search</div>
+            </div>
+            <div className="flex items-center space-x-4">
+              <label htmlFor="filterDesignation" className="font-medium">Filter by Designation:</label>
+              <select
+                id="filterDesignation"
+                value={designation}
+                onChange={(e)=>{setDesignation(e.target.value)}}
+                className="border border-gray-300 px-2 py-1 rounded"
+              >
+                {dataDesignations&&dataDesignations.map((item, idx) => {
+                return <option key={idx}>{item}</option>;
+              })}
+              </select>
+              <div onClick={()=>{setFilteredData(data)}} className='cursor-pointer'>Clear</div>
+
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            {filteredData &&
+            filteredData.map((item,idx)=>{
+                return (<div className='flex flex-col'>
+                    <div>
+
+                    {item.name}
+                    </div>
+                    <div>
+
+                    {item.id}
+                    </div>
+                    <div>
+                    {item.designation}
+                    </div>
+                    <div>
+                        {item.skills && (
+          <div className="rounded-xl  px-5 py-2 h-max w-max">
+            <div className="font-bold">Skills</div>
+            {item?.skills?.map((item, idx) => {
+              return <div>{item}</div>;
+            })}
+          </div>)}
+                        </div>
+                        <div>
+                        {item?.projects && (
+          <div className="rounded-xl  px-5 py-2">
+            <div className="font-bold">Projects</div>
+            {item?.projects?.map((item, idx) => {
+              return (
+                <div className="flex flex-col text-center border-2 border-black gap-y-2 rounded-2xl my-5">
+                  {item?.name}
+                  <div>{item?.description}</div>
+                  <div>
+                    {item?.tasks && (
+                      <div>
+                        Tasks
+                        <table className="border-2 border-black mx-auto mb-5">
+                          <thead>
+                            <tr>
+                              <td className="px-4">ID</td>
+                              <td className="px-4">Name</td>
+                              <td className="text-center px-4">Status</td>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {item.tasks.map((item, idx) => {
+                              return (
+                                <tr>
+                                  <td className="px-4">{item.id}</td>
+                                  <td className="px-4">{item.name}</td>
+                                  <td className="text-center px-4">
+                                    {item.status}
+                                  </td>
+                                </tr>
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+                    )}
+                  </div>
+                  <div>
+                    {item?.team && (
+                      <div>
+                        Team
+                        <table className="border-2 border-black mx-auto mb-5">
+                          <thead>
+                            <tr>
+                              <td className="px-4">Name</td>
+                              <td className="text-center px-4">Role</td>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {item.team.map((item, idx) => {
+                              return (
+                                <tr>
+                                  <td className="px-4">{item.name}</td>
+                                  <td className="px-4">{item.role}</td>
+                                </tr>
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+                            </div>
+                </div>)
+            })
+        }
+          </div>
+          {/* <div className="mt-8">
+            <h2 className="text-lg font-bold mb-4">Projects Table</h2>
+            <table className="min-w-full bg-white border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 border-b">Project Name</th>
+                  <th className="px-4 py-2 border-b">Team Size</th>
+                  <th className="px-4 py-2 border-b">Completed Tasks</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data &&
+                  Object.entries(data).map(([key, value]) => (
+                    <tr key={key} className="border-b">
+                      <td className="px-4 py-2">{key}</td>
+                      <td className="px-4 py-2">{value.team_size}</td>
+                      <td className="px-4 py-2">{value.completed_tasks}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div> */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default JsonData;
